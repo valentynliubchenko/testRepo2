92fdbd9d07fb97b84d2223f06db327fed575fee5
valentynliubchenko <luybchenko.val@gmail.com>
2024-03-08 11:46:54 +0200

    [EDU] commit 4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println( "Hello World3!" );
         System.out.println( "Hello World4!" );
         System.out.println( "Hello World5!" );
+        System.out.println( "Hello World6!" );
     }
 }
